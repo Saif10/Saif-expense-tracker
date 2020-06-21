f853ecbd91300f51361a7d6ff4f4e357f58efdeb
Saif10 <51514618+Saif10@users.noreply.github.com>
2020-06-22 00:34:26 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './App.css';
+
+//Import Components
+import {Header} from './components/Header'
+import {Balance} from './components/Balance'
+import {AccountSummary} from './components/AccountSummary'
+import {TransactionHistory} from './components/TransactionHistory'
+import {AddTransaction} from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
+
+
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header></Header>
+
+      <div className="container">
+        <Balance />
+        <AccountSummary />
+        <TransactionHistory />
+        <AddTransaction />
+        
+      </div>
+
+     
+      
+    </GlobalProvider>
+  );
+}
+
+export default App;
